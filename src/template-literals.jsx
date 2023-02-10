19e978ff2428f4dd7cf0ea9022c5155918320dc4
AbdulMoeez21 <moizrao7@gmail.com>
2023-02-11 02:14:36 +0500

    Day 2

--- src/template-literals.jsx
+++ src/template-literals.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
 import './index.css'
 
  const fname="Rao"
  const mname="Abdul"
  const lname="Moiz"
  const curr_date=new Date().toLocaleDateString();
- const curr_time=new Date().toLocaleDateString();
+ const curr_time=new Date().toLocaleTimeString();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <> 
@@ -15,14 +13,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 <p> lorem lorem lorem lorem lorem </p> */}
 {/* <h1>{`my  full name is ${fname} ${lname}`}</h1> */}
 {/* Challenge One */}
-{/* <h1>Netflix Movies</h1>
+ <h1>Netflix Movies</h1>
 <p>List of 5 Best Series</p>
 <ol>
   <li>Dark</li>
   <li>13 Reasons Why</li>
   <li>Sacred Games</li>
   <li>Mirzapur</li>
-</ol> */}
+</ol> 
 {/* Challenge TWO */}
 <h1>{`My name is ${fname} ${mname} ${lname}`}</h1>
 <p>{`Today's Date is "${curr_date}"`}</p>
