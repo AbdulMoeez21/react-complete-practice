f88a6af2c496bcb210a347d129e6911bd6f394b4
AbdulMoeez21 <moizrao7@gmail.com>
2023-02-11 00:25:18 +0500

    Day 2

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,6 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<> <h1>Hello world!!</h1>
+<> <h1>Hello world!</h1>
 <p> lorem lorem lorem lorem lorem </p></>
 )
